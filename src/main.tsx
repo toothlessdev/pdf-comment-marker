dfd21097118c1bdae5de48fac7352f0e5d5da4d9
toothlessdev <toothless042@gmail.com>
2025-03-24 00:43:11 +0900

    chore: initialize project

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import "reflect-metadata";
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import { App } from "./App";
+import "./style.css";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
